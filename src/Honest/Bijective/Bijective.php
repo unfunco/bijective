d2f480ed33beb194f2c6682cfea2b610575865cb
Daniel Morris <daniel@honestempire.com>
2013-11-10 15:12:39 +0000

    Use the array() syntax instead of []
    This will then allow older versions of PHP to be supported

--- src/Honest/Bijective/Bijective.php
+++ src/Honest/Bijective/Bijective.php
@@ -53,7 +53,7 @@ class Bijective
      * @static
      * @access private
      */
-    static private $_chars = [
+    static private $_chars = array(
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
         'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
@@ -61,7 +61,7 @@ class Bijective
         'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
         'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
         '8', '9',
-    ];
+    );
 
     /**
      * Encode an integer into the corresponding bijective string
