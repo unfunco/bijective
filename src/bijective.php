3d4251dca1a2bcc5210541ef81599d055f8fd99e
Daniel Morris <daniel@honestempire.com>
2016-01-23 10:00:10 +0000

    Optimise the library for reads

--- src/bijective.php
+++ src/bijective.php
@@ -42,11 +42,13 @@ namespace Honest\Bijective
      * @var array
      */
     const ELEMENTS = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
-        'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
-        '4', '5', '6', '7', '8', '9',
+        'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7, 'i' => 8, 'j' => 9, 'k' => 10,
+        'l' => 11, 'm' => 12, 'n' => 13, 'o' => 14, 'p' => 15, 'q' => 16, 'r' => 17, 's' => 18, 't' => 19, 'u' => 20,
+        'v' => 21, 'w' => 22, 'x' => 23, 'y' => 24, 'z' => 25, 'A' => 26, 'B' => 27, 'C' => 28, 'D' => 29, 'E' => 30,
+        'F' => 31, 'G' => 32, 'H' => 33, 'I' => 34, 'J' => 35, 'K' => 36, 'L' => 37, 'M' => 38, 'N' => 39, 'O' => 40,
+        'P' => 41, 'Q' => 42, 'R' => 43, 'S' => 44, 'T' => 45, 'U' => 46, 'V' => 47, 'W' => 48, 'X' => 49, 'Y' => 50,
+        'Z' => 51, '0' => 52, '1' => 53, '2' => 54, '3' => 55, '4' => 56, '5' => 57, '6' => 58, '7' => 59, '8' => 60,
+        '9' => 61,
     ];
 
     /**
@@ -59,10 +61,11 @@ namespace Honest\Bijective
     function bijective_encode(int $input): string
     {
         $encoded = '';
+        $elements = array_flip(ELEMENTS);
 
         do {
             $modulus = $input % 62;
-            $encoded = ELEMENTS[$modulus] . $encoded;
+            $encoded = $elements[$modulus] . $encoded;
 
             $input = ($input - $modulus) / 62;
         } while ($input > 0);
@@ -80,10 +83,9 @@ namespace Honest\Bijective
     function bijective_decode(string $input): int
     {
         $decoded = 0;
-        $elements = array_flip(ELEMENTS);
 
         for ($i = $length = strlen($input); $i--;) {
-            $decoded = $elements[$input{$i}] * 62 ** ($length - $i - 1) + $decoded;
+            $decoded = ELEMENTS[$input{$i}] * 62 ** ($length - $i - 1) + $decoded;
         }
 
         return $decoded;
